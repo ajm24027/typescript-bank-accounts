e21ba54e15895a83e5de39bb6815152d6d0c3fe8
Anthony Medina <anthony.john.medina@gmail.com>
2023-08-16 21:07:13 -0400

    Section 3 completed up to Favorite Accounts

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-console.log("random", Math.random());
+// I need to be able to add money to a bank account. +
+// I need to be able to withdraw money from a bank account. +
+// I need to be able to get the current balance. +
+// I need to throw an error if there isn't enough funds. + 
+// I need to be able to transfer money to another account.
+// I need to be able to "note" the mensual interest ceiling - if the interest rate is 0.01 (1%), and I have $1000 in my account, the interest noted should be $10. +
+// Additionally, there should be a maximium of $50,000 in interested calculated. Which means if I have $52,000 in my account, I should still note that the mensual interest is still 1% of $50,000. +
+// I should be able to acccess a list of my favorite accounts, add an account to said list, and remove an account from the list.
+var BankAccount = /** @class */ (function () {
+    function BankAccount(balance, name) {
+        if (balance === void 0) { balance = 0; }
+        this.balance = balance;
+        this.name = name;
+    }
+    BankAccount.prototype.addFunds = function (deposit) {
+        var newBalance = this.balance + deposit;
+        this.balance = newBalance;
+        console.log("".concat(this.name, ": You deposited: ").concat(deposit, ". Your new balance is: ").concat(newBalance));
+    };
+    BankAccount.prototype.withdrawFunds = function (withdrawal) {
+        if (withdrawal > this.balance) {
+            console.log("".concat(this.name, " has insufficient funds."));
+        }
+        else {
+            var newBalance = this.balance - withdrawal;
+            this.balance = newBalance;
+            console.log("You withdrew $".concat(withdrawal, " from your account - ").concat(this.name, ". Your new balance is: ").concat(this.balance));
+        }
+    };
+    BankAccount.prototype.checkBalance = function () {
+        console.log("The balance of your account - ".concat(this.name, " is: ").concat(this.balance));
+    };
+    BankAccount.prototype.checkIRR = function () {
+        // The Interest Rate a Customer recieves is 1%, and should cap out at $50,000.
+        var ROI = (this.balance * (0.01 * 10)) / 10;
+        if (ROI >= 50000) {
+            console.log("You've capped out your interest rate return at $50,000.");
+        }
+        else {
+            console.log("Your current IRR is: ".concat(ROI, "."));
+        }
+    };
+    return BankAccount;
+}());
+var latestAccount = new BankAccount(0, "AJ's First Bank Account");
+console.log("Your new account - \"".concat(latestAccount.name, "\" - has been created!"));
+latestAccount.addFunds(100);
+latestAccount.checkBalance();
+latestAccount.withdrawFunds(50);
+latestAccount.checkIRR();
+latestAccount.checkBalance();
--- src/index.ts
+++ src/index.ts
@@ -5,41 +5,67 @@
 // I need to be able to transfer money to another account.
 
 
-// I need to be able to "note" the mensual interest ceiling - if the interest rate is 0.01 (1%), and I have $1000 in my account, the interest noted should be $10. 
-// Additionally, there should be a maximium of $50,000 in interested calculated. Which means if I have $52,000 in my account, I should still note that the mensual interest is still 1% of $50,000.
+// I need to be able to "note" the mensual interest ceiling - if the interest rate is 0.01 (1%), and I have $1000 in my account, the interest noted should be $10. +
+// Additionally, there should be a maximium of $50,000 in interested calculated. Which means if I have $52,000 in my account, I should still note that the mensual interest is still 1% of $50,000. +
 
 // I should be able to acccess a list of my favorite accounts, add an account to said list, and remove an account from the list.
 
 class BankAccount {
-  public balance : number
-  public name: string
+  private balance : number
+  private name: string
 
-  constructor(balance: number = 0, name: string) {
+  constructor(balance: number, name: string) {
     this.balance = balance
     this.name = name
 }
 
   addFunds(deposit: number) {
-   const newBalance = this.balance + deposit
+   const newBalance = this.balance += deposit
    this.balance = newBalance
-   console.log(`${this.name}: You deposited: ${deposit}. Your new balance is: ${newBalance}`)
+   console.log(`${this.name}: You deposited: $${deposit}. Your new balance is: ${newBalance}`)
   }
 
   withdrawFunds(withdrawal: number) {
     if (withdrawal > this.balance) {
       console.log(`${this.name} has insufficient funds.`);
     } else {
-      const newBalance = this.balance - withdrawal;
+      const newBalance = this.balance -= withdrawal;
       this.balance = newBalance;
-      console.log(`You withdrew $${withdrawal} from your account - ${this.name}. Your new balance is: ${this.balance}`);
+      console.log(`You withdrew $${withdrawal} from your account - ${this.name}. Your new balance is: $${this.balance}`);
     }
   }
   
   checkBalance() {
-    console.log(`The balance of your account - ${this.name} is: ${this.balance}`)
+    console.log(`The balance of your account - ${this.name} is: $${this.balance}`)
   }
 
-  transferFunds(accountName: string, funds: number) {
-    
+  // For actual use, when you use return statement in a function, you must add the type to the function to declare what the expected type of return is - versus when we just use console.log().
+
+  // getBalance(): number {
+  //   return this.balance
+  // }
+  
+transferMoney(amount: number, account: BankAccount) {
+  if (amount > this.balance) {
+    console.log('This account has insufficient funds!')
+  } else {
+    account.addFunds(amount)
+    console.log(`${amount} was deposited to your bank account - ${account}`)
   }
+}
+
+  checkIRR() {
+    // The Interest Rate a Customer recieves is 1%, and should cap out at $50,000.
+    const ROI: number = (this.balance * (0.01 * 10)) / 10
+
+    if (ROI >= 50000) {
+      console.log(`You've capped out your interest rate return at $50,000.`)
+    } else {
+      console.log(`Your current IRR is: $${ROI}.`)
+    }
+  }
+
+  // transferFunds(accountName: string, funds: number) {
+    
+  // }
 }
